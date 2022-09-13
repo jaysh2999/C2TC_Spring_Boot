989b3965fa0ed0df7374a972a36c3542832e5816
mayur <mayur@LAPTOP-FLGR9891>
2022-09-13 17:03:58 +0530

    second

---
+++ ProductModule/src/test/java/com/tns/product/ProductModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tns.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
